80845ee5a7ff79fbcd93c5cf93822a0468274bb4
Harish <harish.balasubramanian@zohocorp.com>
2022-07-26 20:24:44 +0530

    6.2 done

--- week-6-assignment/6.2.js
+++ week-6-assignment/6.2.js
@@ -1,31 +1,54 @@
-function spiralMatrix(m, n, mat) {
-    let i, k = 0; l = 0;
-    let last_row = m -1; last_col = n-1;
-    while(k <= last_row && l <= last_col) {
-        for(i = l; i <= last_col; i++) {
-            console.log(mat[k][i]);
-        }
-        k++;
+const spiralMatrix = function (matrix) {
+	const arr = []
 
-        for(let i = k; i <= last_row; i++) {
-            console.log(mat[i][last_col]);
-        }
-        last_col--;
-        if(k <= last_row) {
-            for(let i = last_col; i >= l; i++) {
-                console.log(mat[last_row][i]);
-            }
-            last_row--;
-        }
+	//set the bounds of the matrix
+	let rowTop = 0
+	let rowBottom = matrix.length - 1
+	let colLeft = 0
+	let colRight = matrix[0].length - 1
 
-        if(l <= last_col) {
-            for(let i = last_row; i >= k; i--) {
-                console.log(mat[i][l])
-            }
-            l++
-        }
-    }
-}
+	const matrixSize = matrix.length * matrix[0].length
+
+	//while there are still more elements in the matrix not in the resultant array
+	while (arr.length < matrixSize) {
+		//iterate from bound to bound in a spiral pattern and push each element to the array
+
+		//iterate right through the top row
+		for (let col = colLeft; col <= colRight; col++) {
+			arr.push(matrix[rowTop][col])
+		}
+		//lower the top row bound
+		rowTop++
+
+		//iterate down through the right column
+		for (let row = rowTop; row <= rowBottom; row++) {
+			arr.push(matrix[row][colRight])
+		}
+		//move the right column bound left
+		colRight--
+
+		//if every element has been iterated over, end the loop
+		if (arr.length >= matrixSize) {
+			break
+		}
+
+		//iterate left through the bottom row
+		for (let col = colRight; col >= colLeft; col--) {
+			arr.push(matrix[rowBottom][col])
+		}
+		//raise the bottom row bound
+		rowBottom--
+
+		//iterate up through the left column
+		for (let row = rowBottom; row >= rowTop; row--) {
+			arr.push(matrix[row][colLeft])
+		}
+		//move the left column bound to the right
+		colLeft++
+	}
+	//return the resultant array
+	return arr
+};
 
 const arr = [["a", "b", "c", "d", "e"], ["f", "g", "h", "i", "j"], ["u", "v", "m", "n", "o"], ["p", "q", "r", "s", "t"]];
-spiralMatrix(4, 5, arr)
+console.log(spiralMatrix(arr))
